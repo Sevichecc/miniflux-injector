172507ff0a9b26d7d1f43bba23b358d0697abe74
sevichecc <seviche@seviche.cc>
2022-12-14 23:10:10 +0800

    refactor: modify the style of searchInjection box

--- src/searchInjection.js
+++ src/searchInjection.js
@@ -1,5 +1,5 @@
 function isChrome() {
-  return typeof chrome !== "undefined";
+  return typeof chrome !== 'undefined';
 }
 
 function getBrowser() {
@@ -10,41 +10,41 @@ function getBrowser() {
   through miniflux search results, e.g. in the bookmark title or description 
 */
 function escapeHTML(str) {
-  let p = document.createElement("p");
+  let p = document.createElement('p');
   p.appendChild(document.createTextNode(str));
   return p.innerHTML;
 }
 
 const browser = getBrowser();
 
-let port = browser.runtime.connect({ name: "port-from-cs" });
+let port = browser.runtime.connect({ name: 'port-from-cs' });
 let searchEngine;
 if (document.location.hostname.match(/duckduckgo/)) {
-  searchEngine = "duckduckgo";
+  searchEngine = 'duckduckgo';
 } else if (document.location.hostname.match(/google/)) {
-  searchEngine = "google";
+  searchEngine = 'google';
 }
 
 // When background script answers with results, construct html for the result box
 port.onMessage.addListener(function (m) {
   const parser = new DOMParser();
   let theme, themeClass;
-  let htmlString = "";
+  let htmlString = '';
   let html;
 
   // In case we don't get results, but a message from the background script,
   // display it. This is the case before proper configuration
-  if ("message" in m) {
+  if ('message' in m) {
     htmlString = `
     <div id="bookmark-list-container" class="${searchEngine}">
       <div id="navbar">
-        <a id="ld-logo">  
-          <img src=${browser.runtime.getURL("icons/logo.svg")} />
-          <h1>miniflux injector</h1>
-        </a>
-        <a id="ld-options" class="openOptions">
+        <span id="mf-logo">  
+           <img src=${browser.runtime.getURL('icons/logo_full.svg')}
+           alt="miniflux injector"/>
+        </span>
+        <a id="mf-options" class="openOptions">
           <img class="ld-settings" src=${browser.runtime.getURL(
-            "icons/cog.svg"
+            'icons/cog.svg'
           )} />
         </a>
       </div>
@@ -55,7 +55,7 @@ port.onMessage.addListener(function (m) {
     `;
 
     // Convert the above string into a DOM document
-    html = parser.parseFromString(htmlString, "text/html");
+    html = parser.parseFromString(htmlString, 'text/html');
   }
   // If there is no message and there are actual results display them
   else if (m.results.length > 0) {
@@ -63,15 +63,15 @@ port.onMessage.addListener(function (m) {
     // specific CSS classes
     // Get the theme configuration
     switch (searchEngine) {
-      case "duckduckgo":
+      case 'duckduckgo':
         theme = m.config.themeDuckduckgo;
         break;
-      case "google":
+      case 'google':
         theme = m.config.themeGoogle;
         break;
     }
-    if (theme == "auto") {
-      themeClass = ""; // automatic theme detection
+    if (theme == 'auto') {
+      themeClass = ''; // automatic theme detection
     } else {
       themeClass = theme; // "dark" for dark theme, "light" for light theme
     }
@@ -79,23 +79,24 @@ port.onMessage.addListener(function (m) {
     // URL of the configured miniflux instance (including search term)
     let minifluxUrl =
       m.config.baseUrl +
-      (searchTerm.length > 0 ? `/v1/entries?search=${searchTerm}` : "/");
+      (searchTerm.length > 0 ? `/search?q=${searchTerm}` : '/');
 
     htmlString += `
     <div id="bookmark-list-container" class="${searchEngine} ${themeClass}">
       <div id="navbar">
-        <a id="ld-logo" href="${minifluxUrl}">  
-          <img src=${browser.runtime.getURL("icons/logo.svg")} />
-          <h1>miniflux injector</h1>
-        </a>
-        <div id="results_amount">
+        <span id="mf-logo">  
+          <img src=${browser.runtime.getURL(
+            'icons/logo_full.svg'
+          )} alt="miniflux injector"/>
+        </span>
+        <div id="mf-results_amount">
           Found <span>${m.results.length}</span> ${
-      m.results.length == 1 ? "result" : "results"
+      m.results.length == 1 ? 'result' : 'results'
     }.
         </div>
-        <a id="ld-options" class="openOptions">
+        <a id="mf-options" class="openOptions">
           <img class="ld-settings" src=${browser.runtime.getURL(
-            "icons/cog.svg"
+            'icons/cog.svg'
           )} />
         </a>
       </div>
@@ -108,32 +109,37 @@ port.onMessage.addListener(function (m) {
         <li>
           <div class="title">
             <a
-              href="${entry.url}"
-              target=${m.config.openNewTab ? "_blank" : "_self"}
+              href="${entry.baseUrl}"
+              target=${m.config.openNewTab ? '_blank' : '_self'}
               rel="noopener"
+              id="mf-title"
               >${escapeHTML(entry.title)}</a
             >
           </div>
           <div class="description ${themeClass}">
-            <span class="tags">
-             <a> ${escapeHTML(entry.author)} </a>
-            </span>
+             ${escapeHTML(entry.author)}
           </div>
         </li>`;
     });
-    htmlString += `</ul></div>`;
+    htmlString += `
+        </ul>
+        <div id="more-links"> 
+          <a href="${minifluxUrl}">More</a>
+        </div>
+      </div>
+        `;
   } else {
-    console.error("miniflux injector: no message and no search results");
+    console.error('miniflux injector: no message and no search results');
     return;
   }
 
   let sidebar; // DOM document for the sidebar
 
   // querySelectors for finding the sidebar in the search engine websites
-  if (searchEngine == "duckduckgo") {
-    sidebar = document.querySelector(".sidebar-modules");
-  } else if (searchEngine == "google") {
-    sidebar = document.querySelector("#rhs");
+  if (searchEngine == 'duckduckgo') {
+    sidebar = document.querySelector('.sidebar-modules');
+  } else if (searchEngine == 'google') {
+    sidebar = document.querySelector('#rhs');
     if (sidebar == null) {
       // google completely omits the sidebar container if there is no content.
       // we need to add it manually before injection
@@ -143,24 +149,24 @@ port.onMessage.addListener(function (m) {
       // construct DOM document from string
       let sidebarContainer = parser.parseFromString(
         sidebarContainerString,
-        "text/html"
+        'text/html'
       );
-      let container = document.querySelector("#rcnt"); // get main search result container
-      container.appendChild(sidebarContainer.body.querySelector("div"));
-      sidebar = document.querySelector("#rhs"); // get the added sidebar container
+      let container = document.querySelector('#rcnt'); // get main search result container
+      container.appendChild(sidebarContainer.body.querySelector('div'));
+      sidebar = document.querySelector('#rhs'); // get the added sidebar container
     }
   }
 
   // Convert the html string into a DOM document
-  html = parser.parseFromString(htmlString, "text/html");
+  html = parser.parseFromString(htmlString, 'text/html');
   // The actual injection
-  sidebar.prepend(html.body.querySelector("div"));
+  sidebar.prepend(html.body.querySelector('div'));
 
   // Event listeners for opening the extension options. These can only be opened
   // by the background script, so we need to send a message to it
-  document.querySelectorAll(".openOptions").forEach((el) => {
-    el.addEventListener("click", () => {
-      port.postMessage({ action: "openOptions" });
+  document.querySelectorAll('.openOptions').forEach((el) => {
+    el.addEventListener('click', () => {
+      port.postMessage({ action: 'openOptions' });
     });
   });
 });
@@ -168,6 +174,6 @@ port.onMessage.addListener(function (m) {
 // Start the search by sending a message to background.js with the search term
 let queryString = location.search;
 let urlParams = new URLSearchParams(queryString);
-let searchTerm = urlParams.get("q");
+let searchTerm = urlParams.get('q');
 
 port.postMessage({ searchTerm: searchTerm });
